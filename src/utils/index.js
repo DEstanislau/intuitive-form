dbe1b677d777c16afea29851519455ecf82e3c8b
Diego Estanislau <diegoperrone2018@gmail.com>
2021-04-09 23:12:02 -0300

    unused hooks removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,17 +11,4 @@ const getValidationErrors = err => {
   return validationErrors;
 };
 
-const maskCep = value => {
-  const firstValue = value.replace(/\D/g, ''); // 1239856
-  const finalValue = firstValue.replace(/^(\d{5})(\d)/, '$1-$2');
-  return finalValue;
-};
-
-const maskPhone = value => {
-  const firstValue = value.replace(/\D/g, '');
-  const secondValue = firstValue.replace(/^(\d{2})(\d)/g, '($1)$2');
-  const finalValue = secondValue.replace(/(\d)(\d{4})$/, '$1-$2');
-  return finalValue;
-};
-
-export { normalizeText, getValidationErrors, maskPhone, maskCep };
+export { normalizeText, getValidationErrors };
